0d2307441bcb009bceff97fd4ade8f2427f8d7a1
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2024-04-11 15:51:17 -0500

    Update test_optim.py

--- tests/test_optim.py
+++ tests/test_optim.py
@@ -200,7 +200,7 @@ def test_history_to_df_pruned(models):
 
     df = history_to_df(result.history["position"])
 
-    assert df.shape == (77, 4)
+    assert df.shape == (84, 4)
 
 
 def test_generate_batches():
