fc8f7861d82e8427fa7a9e60c41489be7a3021b6
Jinbo1 <kingjinbo@hotmail.com>
2024-05-24 13:53:19 +0200

    Update survival

--- liesel_ptm/survival.py
+++ liesel_ptm/survival.py
@@ -1,47 +1,11 @@
 from __future__ import annotations
+
 import jax.numpy as jnp
 import liesel.model as lsl
 from liesel.model.nodes import no_model_setter
-from .custom_types import Array, TFPDistribution
-import liesel_ptm as ptm
-from jax import random
-import scipy.stats
-import numpy as np
 
+from liesel_ptm.custom_types import Array, KeyArray, TFPDistribution
 
-
-class SurvivalTransformationDist(lsl.Dist):
-    "A transformation-distribution node for a survival likelihood"
-
-
-# Generate survival data (easy case)
-key = random.key(0) # generate PRNG keys
-shape = ptm.sample_shape(key, nshape=8).sample # set shape vector
-x = random.uniform(key) # generate uniformly distributed covariate
-ln_y = ptm.PTMLocScaleDataGen(shape = shape,loc_fn= lambda x: x, ncov=1) # set DGP
-ln_y_sample = ptm.PTMLocScaleDataGen.sample(key=key, self=ln_y, nobs= 100) # draw ln(y)
-#y = jnp.exp(ln_y_sample["y"]) # compute duration times by exp(ln(y))
-y = np.exp(ln_y_sample["y"])
-
-# Create indicators of censored data
-c = scipy.stats.bernoulli.rvs(p = 0.05, size = 100)
-
-# draw censoring time
-u = scipy.stats.uniform.rvs(loc = 0.001, scale = y - 0.0001, size = 100) # draw 100 samples of sencoring time
-
-y_hat = np.copy(y)
-y_hat[c ==1] = u[c==1] # set y_hat as y with censored data u when c = 1
-    
-
-
-
-
-
-class TransDistSur(lsl.Dist):
-    def __init__(
-            self,
-            transformed_variable: lsl.Calc
-    )
 
 class TransformationDist(lsl.Dist):
     """A transformation-distribution node for a conditional transformation model."""
@@ -95,3 +59,82 @@ class TransformationDist(lsl.Dist):
         self._outdated = False
         return self
 
+
+##############################################################################
+
+
+# Update the calss to fit survival data
+class TransformationDist_sur(lsl.Dist):
+    """A transformation-distribution node for a conditional transformation model."""
+
+    def __init__(
+        self,
+        transformed_variable: lsl.Calc,  # transformed variables
+        transformation_derivative: lsl.Calc,  # derivative of the transformation function
+        refdist: TFPDistribution,  # reference distribution
+        censoring_indicator: Array,  # A censoring indicator for ecah observation
+        _name: str = "",
+        _needs_seed: bool = False,  # optional parameters for naming the node and specifying a seed
+    ):
+        super(lsl.Dist, self).__init__(
+            transformed_variable,
+            transformation_derivative,
+            _name=_name,
+            _needs_seed=_needs_seed,
+        )
+
+        self._per_obs = True
+        self.refdist = refdist
+        self._transformed_variable = transformed_variable
+        self._transformation_derivative = transformation_derivative
+        self.censoring_indicator = censoring_indicator  # Store censoring type
+
+    @property
+    def log_prob(self) -> Array:
+        """The log-probability of the distribution."""
+        return self.value  # returns the log-probability of the distribution
+
+    @property
+    def per_obs(self) -> bool:
+        """Whether the log-probability is stored per observation or summed up."""
+        return (
+            self._per_obs
+        )  # indicates whether the log-probability should be stored per observation or summed over all observations
+
+    @per_obs.setter  # sets the `per_obs` property
+    @no_model_setter
+    def per_obs(self, per_obs: bool):
+        self._per_obs = per_obs
+
+    def right_censored_log_prob(self, transformed_variable):
+        return jnp.log(
+            1 - self.refdist.cdf(transformed_variable)
+        )  # Use the complementary CDF for right censoring
+
+    def uncensored_log_prob(
+        self, transformed_variable, deriv
+    ):  # calculates log-probability for uncensored data
+        base_log_prob = self.refdist.log_prob(transformed_variable)
+        deriv = jnp.maximum(deriv, 1e-30)
+        log_prob_adjustment = jnp.log(deriv)
+        return jnp.add(base_log_prob, log_prob_adjustment)
+
+    def update(self) -> TransformationDist_sur:  # computes log-prob seperately
+        transformed_variable = self._transformed_variable.value
+        deriv = self._transformation_derivative.value
+
+        # Calculate log-probabilities for censored and uncensored data
+        right_censored_log_prob = self.right_censored_log_prob(transformed_variable)
+        uncensored_log_prob = self.uncensored_log_prob(transformed_variable, deriv)
+
+        # Combine log-probabilities based on the censoring indicator
+        log_prob = jnp.where(
+            self.censoring_indicator == 1, right_censored_log_prob, uncensored_log_prob
+        )  # select approperate log-prob based on censoring indicator
+
+        if not self.per_obs and hasattr(log_prob, "sum"):
+            log_prob = log_prob.sum()  # sums the log-prob if `per_obs` is `False`
+
+        self._value = log_prob
+        self._outdated = False
+        return self
